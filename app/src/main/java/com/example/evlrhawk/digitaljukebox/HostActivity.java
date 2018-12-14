5867cb73bc7206e1cef88c1f838354248f5b9914
bigginlasley <bigginlasley@gmail.com>
2018-12-13 21:22:18 -0700

    made play work
    I fixed play

--- app/src/main/java/com/example/evlrhawk/digitaljukebox/HostActivity.java
+++ app/src/main/java/com/example/evlrhawk/digitaljukebox/HostActivity.java
@@ -16,6 +16,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
@@ -26,6 +28,11 @@ import java.util.List;
 
 public class HostActivity extends AppCompatActivity {
     private final String TAG = "HostActivity";
+
+
+    private static final String CLIENT_ID = "27ead52d8b6d426a85b5a01cd63b388c";
+    private static final int REQUEST_CODE = 1337;
+    private static final String REDIRECT_URI = "com.example.evlrhawk.digitaljukebox://callback";
 
     private Button btnPull, send, play;
 
@@ -73,6 +80,39 @@ public class HostActivity extends AppCompatActivity {
                 showData();
             }
         });
+
+
+//        Button guestBtn = (Button) findViewById(R.id.guestBtn);
+//        guestBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View guestView){
+//                startActivity(new Intent(MainActivity.this, GuestActivity.class));
+//            }
+//        });
+
+
+        Button playBtn = (Button) findViewById(R.id.btnHostPlay);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View playView) {
+                mSpotifyAppRemote.getPlayerApi().play("spotify:album:3JfSxDfmwS5OeHPwLSkrfr");
+                // Subscribe to PlayerState
+
+
+                mSpotifyAppRemote.getPlayerApi()
+                        .subscribeToPlayerState()
+                        .setEventCallback(new Subscription.EventCallback<PlayerState>() {
+                            public void onEvent(PlayerState playerState) {
+                                final Track track = playerState.track;
+                                if (track != null) {
+                                    Log.d("MainActivity", track.name + " by " + track.artist.name);
+                                }
+                            }
+                        });
+            }
+        });
+
+
     }
 
     /**
@@ -170,5 +210,32 @@ public class HostActivity extends AppCompatActivity {
                 });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+                        // Now you can start interacting with App Remote
+//                        connected();
+                    }
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MyActivity", throwable.getMessage(), throwable);
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
 
 }
