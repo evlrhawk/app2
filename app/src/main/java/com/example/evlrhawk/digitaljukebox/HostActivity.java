5a20b7e87e3227639183415ff136f77a6cc89cae
Christopher <wil15090@byui.edu>
2018-12-13 19:35:21 -0700

    added to HostActivity

--- app/src/main/java/com/example/evlrhawk/digitaljukebox/HostActivity.java
+++ app/src/main/java/com/example/evlrhawk/digitaljukebox/HostActivity.java
@@ -2,8 +2,14 @@ package com.example.evlrhawk.digitaljukebox;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HostActivity extends AppCompatActivity {
 
@@ -11,14 +17,55 @@ public class HostActivity extends AppCompatActivity {
 
     // to be used when making our queue in FireBase
     DatabaseReference databaseReference;
+    private EditText URI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
 
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("track");
+        URI = (EditText) findViewById(R.id.sendString);
         btnPull = (Button) findViewById(R.id.btnHostPull);
         send = (Button) findViewById(R.id.btnHostSend);
         play = (Button) findViewById(R.id.btnHostPlay);
+    }
+
+
+    /**
+     * Adds a string to our firebase database
+     *
+     * Written by Christopher Wilson
+     */
+
+    public void addString() {
+        final String TAG = "From addString()";
+
+        Log.i(TAG, "Call to addString()");
+        // the string to be sent
+        String fromApp = URI.getText().toString();
+
+        // if our text entry has something in it to send
+        if (!TextUtils.isEmpty(fromApp)) {
+            Log.v(TAG, "Sent to \"full\" 'if' statement");
+            // get the database reference id
+            String id = databaseReference.push().getKey();
+            // make a new object to send to the database
+            ToSend toSend1 = new ToSend(fromApp);
+
+            // send the value to the database
+            databaseReference.child(id).setValue(toSend1);
+
+            Log.i(TAG, "Sent to firebase");
+
+        }
+        // if we don't have anything in our text entry box
+        else {
+            Log.v(TAG, "Sent to \"blank\" 'if' statement");
+
+            // tell them to put something in
+            Toast.makeText(HostActivity.this, "Please type a string to send.", Toast.LENGTH_LONG);
+        }
     }
 }
--- app/src/main/java/com/example/evlrhawk/digitaljukebox/host.java
+++ app/src/main/java/com/example/evlrhawk/digitaljukebox/host.java
@@ -2,12 +2,64 @@ package com.example.evlrhawk.digitaljukebox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class host extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
+    private EditText string;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("track");
+        string = (EditText) findViewById(R.id.sendString);
+        btnSend = (Button) findViewById(R.id.hostBtn);
+    }
+
+    /**
+     * Adds a string to our firebase database
+     *
+     * Written by Christopher Wilson
+     */
+
+    public void addString() {
+        final String TAG = "From addString()";
+
+        Log.i(TAG, "Call to addString()");
+        // the string to be sent
+        String fromApp = string.getText().toString();
+
+        // if our text entry has something in it to send
+        if (!TextUtils.isEmpty(fromApp)) {
+            Log.v(TAG, "Sent to \"full\" 'if' statement");
+            // get the database reference id
+            String id = databaseReference.push().getKey();
+            // make a new object to send to the database
+            ToSend toSend1 = new ToSend(fromApp);
+
+            // send the value to the database
+            databaseReference.child(id).setValue(toSend1);
+
+            Log.i(TAG, "Sent to firebase");
+
+        }
+        // if we don't have anything in our text entry box
+        else {
+            Log.v(TAG, "Sent to \"blank\" 'if' statement");
+
+            // tell them to put something in
+            Toast.makeText(MainActivity.this, "Please type a string to send.", Toast.LENGTH_LONG);
+        }
     }
 }
+
